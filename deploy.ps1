d9e6d9005ad2bb4c2e7c253e3d0ff1b58da1b089
James Barlow <jamesbarlow16@gmail.com>
2021-03-07 07:39:40 +0000

    vaulted password

--- deploy.ps1
+++ deploy.ps1
@@ -3,10 +3,10 @@ $templateFile = "C:\temp\repo\Azure-DNS-Test\azuredeploy.json"
 $resourceGroupName = "dns-test-deligation"
 $location = "uksouth"
 
+Connect-AzAccount -Tenant 31c52172-8234-4c52-b8d8-8fcc7696dc9b
 
 New-AzResourceGroup -Name $resourceGroupName -Location $location
-
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile ./azuredeploy.json -templateParameterFile ./azuredeploy.parameters.json
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile ./azuredeploy.json -templateParameterFile ./azuredeploy.parameters.json -Name $projectName
 
 
 
